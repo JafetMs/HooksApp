2d201b69736bf65f35a5e25362be3ad2aadd9c94
JafetMs <reaperkoala260@gmail.com>
2024-10-23 17:40:38 -0600

    Hooks Memos

--- HooksApp/src/main.jsx
+++ HooksApp/src/main.jsx
@@ -16,16 +16,20 @@ import "./index.css";
 // import { MultipleCustomHook } from './03-examples/MultipleCustomHook.jsx'
 
 // ======== 04 - useRef ========== //
-import { FocusScreen } from "./04-useRef/FocusScreen.jsx";
+// import { FocusScreen } from "./04-useRef/FocusScreen.jsx";
 
 // ======== 05 - useLayoutEffect ========== //
-import { Layout } from "./05-useLayoutEffect/Layout.jsx";
+// import { Layout } from "./05-useLayoutEffect/Layout.jsx";
 
 // ======== 06 - useMemorize ========== //
-import { Memorize } from "./06-memos/Memorize.jsx";
+// import { Memorize } from "./06-memos/Memorize.jsx";
+// import { MemoHook } from "./06-memos/MemoHook.jsx";
+// import { CallBackHook } from "./06-memos/CallBackHook.jsx";
+
+import { Padre } from "./07-tarea-memo/Padre.jsx";
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
-  <Memorize />,
+  <Padre/>
   /* </StrictMode>, */
 );
